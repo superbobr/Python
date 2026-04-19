0589ce2ee3a89e2345a83f6f6cc43d1b54299e5b
leonidseoshnik <leonseoprofi@gmail.com>
2026-04-19 14:04:03 +0300

    update formatting.py

--- formatting.py
+++ formatting.py
@@ -1,4 +1,5 @@
 from string import Template
+import datetime
 
 template_str = input()
 user_name = input()
@@ -6,3 +7,9 @@ order_id = input()
 
 t = Template(template_str)
 print(t.substitute(user=user_name, order_id=order_id))
+
+
+event_time = datetime.datetime(2025, 10, 26, 10, 30, 0)
+
+print(f'Дата: {event_time:%d.%m.%Y}')
+print(f'Время события: {event_time:%H:%M:%S}')
