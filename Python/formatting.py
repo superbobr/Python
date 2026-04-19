414188c56f78e9bf7b348650e96aa27189d01e70
leonidseoshnik <leonseoprofi@gmail.com>
2026-04-19 13:36:47 +0300

    fix submodule -> normal folder

---
+++ Python/formatting.py
@@ -0,0 +1,10 @@
+from string import Template
+
+template_str = input()
+user_name = input()
+order_id = input()
+
+t = Template(template_str)
+print(t.substitute(user=user_name, order_id=order_id))
+
+
