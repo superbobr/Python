5cb2c944a2714d4291423bf3c804b6484b28be7b
leonidseoshnik <leonseoprofi@gmail.com>
2026-06-19 19:55:07 +0300

    change education.py

--- education.py
+++ education.py
@@ -1,11 +1,18 @@
-import requests
+from datetime import datetime, date, timedelta
+
+current_data = date(*map(int, input().split('-')))
+day = timedelta(days=int(input()))
+print(current_data + day)
 
 
-r = requests.get("")
-text = r.text
 
-line = text.splitlines()
-y = 0
-for x in line:
-    y += 1
-    print(y)
+
+data = input().split()
+launch_time = datetime(*map(int, data[0].split('-')), *map(int, data[1].split(':')))
+data2 = list(map(int, (input().split())))
+command_time = timedelta(hours=data2[0], minutes=data2[1], seconds=data2[2])
+
+print(launch_time - command_time)
+
+
+
